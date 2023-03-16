ea8d65938593d8c9e11534e30f7d3a42126b9c27
Gawls <matt.gawley@gmail.com>
2023-03-16 01:11:54 -0600

    Updated to reflect scripts going into folders

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,20 @@
 fx_version 'cerulean'
 game 'gta5'
 
+description 'xnTattoos'
+
+shared_script 'config.lua'
+
 client_scripts {
-	'jaymenu.lua',
-	'config.lua',
-	'client.lua'
+	'client/jaymenu.lua',
+	'client/main.lua'
 }
 
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
-	'server.lua'
+	'server/main.lua'
 }
 
 file 'AllTattoos.json'
+
+lua54 'yes'
